a7a9a473d1228b96b5622dec4f215286164e77f7
A-8246 <62061147+A-8246@users.noreply.github.com>
2020-08-26 17:32:14 +0530

    Create Favourite_Byte.py

---
+++ General/Favourite_Byte.py
@@ -0,0 +1,10 @@
+#There is a also an elegent way to do it {ETAOIN SHLRDU}
+
+#But here i am bruteforcing the xor
+
+from pwn import xor
+
+flag = bytes.fromhex("73626960647f6b206821204f21254f7d694f7624662065622127234f726927756d")
+
+for i in range(1,255):
+	print(xor(flag, bytes(chr(i), 'utf-8')))
